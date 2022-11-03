13c7dd0cb16cce997b5c2c49a531ea9423e5b51c
Tom Leary <tlbignerd@gmail.com>
2022-11-03 09:42:25 -0400

    Add execution comments

--- Scripts/Set-Perm.ps1
+++ Scripts/Set-Perm.ps1
@@ -1,3 +1,10 @@
+<#
+Execution Steps:
+
+Import-Module ".\Scripts\Set-Perm.ps1"
+cd  C:\Users\Public\Desktop
+Set-Full-User-Permissions
+#>
 function Set-Full-User-Permissions([string]$path = ".\") {
     $name = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
     $acl = Get-Acl $path
